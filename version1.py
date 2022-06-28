8382a0b17394abc81080662405b87368c698b45e
Gabe Small <gabesmall@dhcp-vl2041-10373.eduroam.cornell.edu>
2022-06-28 14:31:01 -0400

    started the code

---
+++ version1.py
@@ -0,0 +1,6 @@
+#using keras from keras.io which is an extension of tensorflow and matplotlib for graphing
+import tensorflow as ts
+from tensorflow import keras
+from keras import layers
+import matplotlib as mpl
+#need data to continue
